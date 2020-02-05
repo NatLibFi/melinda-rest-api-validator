687b1c4f5a0de8b33983acc39286cf9ce1774821
natlibfi-jonollil <53558796+natlibfi-jonollil@users.noreply.github.com>
2020-02-05 16:25:39 +0200

    Nerfed AMQP to use localhost & no user:password

--- src/config.js
+++ src/config.js
@@ -7,11 +7,11 @@ const {readEnvironmentVariable, parseBoolean} = Utils;
 export const POLL_REQUEST = readEnvironmentVariable('POLL_REQUEST', {defaultValue: 0, format: v => parseBoolean(v)});
 export const POLL_WAIT_TIME = readEnvironmentVariable('POLL_WAIT_TIME', {defaultValue: 1000});
 
-// Rabbit variables
-export const AMQP_URL = readEnvironmentVariable('AMQP_URL', {format: v => JSON.parse(v)});
+// Amqp variables to priority
+export const AMQP_URL = readEnvironmentVariable('AMQP_URL', {defaultValue: 'amqp://127.0.0.1:5672/'});
 
-// Mongo variables
-export const MONGO_URI = readEnvironmentVariable('MONGO_URI', {defaultValue: 'mongodb://localhost:27017/db'});
+// Mongo variables to bulk
+export const MONGO_URI = readEnvironmentVariable('MONGO_URI', {defaultValue: 'mongodb://127.0.0.1:27017/db'});
 
 // SRU variables
 export const SRU_URL_BIB = readEnvironmentVariable('SRU_URL_BIB');
