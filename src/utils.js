2640ef0de4e78035408ac9e01f0790dfba0eae4f
natlibfi-arlehiko <natlibfi-arlehiko@users.noreply.github.com>
2020-10-09 11:47:57 +0300

    Add initial implementation (#16)
    Co-authored-by: natlibfi-jonollil <53558796+natlibfi-jonollil@users.noreply.github.com>

---
+++ src/utils.js
@@ -0,0 +1,19 @@
+import {toAlephId} from '@natlibfi/melinda-commons';
+import {createLogger} from '@natlibfi/melinda-backend-commons';
+
+const logger = createLogger();
+
+export function updateField001ToParamId(id, record) {
+  logger.log('silly', `Updating F001 value to ${id}`);
+  const fields = record.get(/^001$/u);
+
+  if (fields.length === 0) {
+    // Return to break out of function
+    record.insertField({tag: '001', value: toAlephId(id)});
+    return record;
+  }
+
+  fields[0].value = toAlephId(id); // eslint-disable-line functional/immutable-data
+
+  return record;
+}
