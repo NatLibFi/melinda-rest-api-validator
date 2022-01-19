7422a17dc180fdfc2555bd97fdf7681cb6376f19
Minttu Hurme <ammsalme@users.noreply.github.com>
2022-01-19 21:12:41 +0200

    Throw 409 false matcher status and no matches

--- src/interfaces/validator/index.js
+++ src/interfaces/validator/index.js
@@ -180,7 +180,7 @@ export default async function ({formatOptions, sruUrl, matchOptionsList}) {
   }
 
   // eslint-disable-next-line max-statements
-  async function iterateMatchersUntilMatchIsFound({matchers, matchOptionsList, updatedRecord, matcherCount = 0, matcherNoRunCount = 0}) {
+  async function iterateMatchersUntilMatchIsFound({matchers, matchOptionsList, updatedRecord, matcherCount = 0, matcherNoRunCount = 0, matcherFalseZeroCount = 0}) {
 
     const [matcher] = matchers;
     const [matchOptions] = matchOptionsList;
@@ -197,9 +197,29 @@ export default async function ({formatOptions, sruUrl, matchOptionsList}) {
 
       try {
 
+        // matchResults from melinda-record-matching-js v2.1.0
+
+        // matches : array of matching candidate records
+        // nonMatches : array of nonMatching candidate records (if returnNonMatches option is true, otherwise empty array)
+        // - candidate.id
+        // - candidate.record
+        // - probability
+        // - strategy (if returnStrategy option is true)
+        // - treshold (if returnStrategy option is true)
+        // - matchQuery (if returnQuery option is true)
+
+        // we could have here also returnRecords/returnMatchRecords/returnNonMatchRecord options that could be turned false for not to return actual record data
+
+        // matchStatus.status: boolean, true if matcher retrieved and handled all found candidate records, false if it did not
+        // matchStatus.stopReason: string ('maxMatches','maxCandidates','maxedQueries',empty string/undefined), reason for stopping retrieving or handling the candidate records
+        // - only one stopReason is returned (if there would be several possible stopReasons, stopReason is picked in the above order)
+        // - currently stopReason can be non-empty also in cases where status is true, if matcher hit the stop reason when handling the last available candidate record
+
         const matchResults = await matcher(updatedRecord);
-        const matchAmount = matchResults.matches.length;
+
         const {matches, matchStatus} = matchResults;
+        const matchAmount = matches.length;
+
         logger.verbose(`Matcher result: ${JSON.stringify(matchResults)}`);
 
         if (matchAmount > 0) { // eslint-disable-line functional/no-conditional-statement
@@ -212,8 +232,15 @@ export default async function ({formatOptions, sruUrl, matchOptionsList}) {
           return matches;
         }
 
+        // eslint-disable-next-line functional/no-conditional-statement
+        if (matchStatus.status === false) {
+          logger.verbose(`Matcher ${matcherName} resulted in ${matchStatus.status}, stopReason ${matchStatus.stopReason}`);
+          // eslint-disable-next-line no-param-reassign
+          matcherFalseZeroCount += 1;
+        }
+
         logger.debug(`No matching record from matcher ${matcherCount} (${matcherName})`);
-        return iterateMatchersUntilMatchIsFound({matchers: matchers.slice(1), matchOptionsList: matchOptionsList.slice(1), updatedRecord, matcherCount, matcherNoRunCount});
+        return iterateMatchersUntilMatchIsFound({matchers: matchers.slice(1), matchOptionsList: matchOptionsList.slice(1), updatedRecord, matcherCount, matcherNoRunCount, matcherFalseZeroCount});
 
       } catch (err) {
 
@@ -228,7 +255,7 @@ export default async function ({formatOptions, sruUrl, matchOptionsList}) {
             throw new ValidationError(HttpStatus.UNPROCESSABLE_ENTITY, err.message);
           }
 
-          return iterateMatchersUntilMatchIsFound(matchers.slice(1), updatedRecord, matcherCount, matcherNoRunCount);
+          return iterateMatchersUntilMatchIsFound({matchers: matchers.slice(1), matchOptionsList: matchOptionsList.slice(1), updatedRecord, matcherCount, matcherNoRunCount, matcherFalseZeroCount});
         }
 
         // SRU SruSearchErrors are 200-responses that include diagnostics from SRU server
@@ -247,12 +274,19 @@ export default async function ({formatOptions, sruUrl, matchOptionsList}) {
       }
     }
 
-    logger.debug(`All ${matcherCount} matchers handled, ${matcherNoRunCount} did not run`);
+    logger.debug(`All ${matcherCount} matchers handled, ${matcherNoRunCount} did not run.`);
+
     // eslint-disable-next-line functional/no-conditional-statement
     if (matcherNoRunCount === matcherCount) {
       logger.debug(`None of the matchers resulted in candidates`);
       throw new ValidationError(HttpStatus.UNPROCESSABLE_ENTITY, 'Generated query list contains no queries');
     }
+
+    if (matcherFalseZeroCount > 0) {
+      logger.debug(`${matcherFalseZeroCount} matchers returned no matches, but did not check all possible candidates`);
+      throw new ValidationError(HttpStatus.CONFLICT, 'Matcher found too many candidates to check');
+    }
+
     return [];
   }
 
