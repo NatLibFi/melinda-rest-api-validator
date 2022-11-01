b5f61f500147b7473bbd29ad54c0705382ffc61f
Minttu Hurme <ammsalme@users.noreply.github.com>
2022-11-01 13:08:59 +0200

    Add debug to own-authorization

--- src/interfaces/validator/own-authorization.js
+++ src/interfaces/validator/own-authorization.js
@@ -1,6 +1,11 @@
 import httpStatus from 'http-status';
 import {Error as ValidationError} from '@natlibfi/melinda-commons';
 
+import createDebugLogger from 'debug';
+
+const debug = createDebugLogger('@natlibfi/melinda-rest-api-validator:validator:own-authorization');
+const debugData = debug.extend('data');
+
 export default ({ownTags, incomingRecord, existingRecord, recordMetadata, validate}) => {
 
   // Skip this validation if validate: false
@@ -9,6 +14,8 @@ export default ({ownTags, incomingRecord, existingRecord, recordMetadata, valida
   }
 
   const lowTags = getLowTags();
+  debugData(`OwnTags: ${ownTags}`);
+  debugData(`LowTags: ${lowTags}`);
 
   if (lowTags && ownTags && lowTags.some(t => !ownTags.includes(t))) { // eslint-disable-line functional/no-conditional-statement
     throw new ValidationError(httpStatus.FORBIDDEN, {message: 'Own authorization error', recordMetadata});
@@ -18,10 +25,15 @@ export default ({ownTags, incomingRecord, existingRecord, recordMetadata, valida
     if (existingRecord) {
       const incomingTags = get(incomingRecord);
       const existingTags = get(existingRecord);
+      debugData(`IncomingTags: ${incomingTags}`);
+      debugData(`ExistingTags: ${existingTags}`);
 
       const additions = incomingTags.reduce((acc, tag) => existingTags.includes(tag) ? acc : acc.concat(tag), []);
 
       const removals = existingTags.reduce((acc, tag) => incomingTags.includes(tag) ? acc : acc.concat(tag), []);
+
+      debugData(`Additions: ${additions}`);
+      debugData(`Removals: ${removals}`);
 
       // Concat and remove duplicates
       return additions.concat(removals).reduce((acc, tag) => acc.includes(tag) ? acc : acc.concat(tag), []);
