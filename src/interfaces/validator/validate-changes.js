ed92c77b9015a6b09745c8ec9834bc1ae660cd5a
Minttu Hurme <ammsalme@users.noreply.github.com>
2023-02-10 14:10:59 +0200

    Skip changes to f008 creationDate (#117)

--- src/interfaces/validator/validate-changes.js
+++ src/interfaces/validator/validate-changes.js
@@ -30,11 +30,14 @@ import deepEqual from 'deep-eql';
 import {detailedDiff} from 'deep-object-diff';
 import createDebugLogger from 'debug';
 import {normalizeEmptySubfieldsRecord} from '../../utils';
+import {MarcRecord} from '@natlibfi/marc-record';
+
+const debug = createDebugLogger('@natlibfi/melinda-rest-api-validator:validator:validate-changes');
+const debugData = debug.extend('data');
 
 // Checks that the incomingRecord and existingRecord are not identical
+// eslint-disable-next-line max-statements
 export function validateChanges({incomingRecord, existingRecord, validate = true}) {
-  const debug = createDebugLogger('@natlibfi/melinda-rest-api-validator:validator:validate-changes');
-  const debugData = debug.extend('data');
 
   if (validate === false) {
     debug(`Skipping validateChanges. Validate: ${validate}`);
@@ -49,13 +52,10 @@ export function validateChanges({incomingRecord, existingRecord, validate = true
     return {changeValidationResult: true};
   }
 
-
-  // normalizeEmptySubfieldsRecord is an utility function which normalizes all all cases of empty subfield value {code: x, value: ""}, {code: x}, {code: x, value: undefined}  to {value: ""}
-  // this is needed, because there are CAT-fields that have empty value in subfield $b
-  // we could optimize this step out, if we could be sure that empty subfield values are normalized somewhere else
-
-  const normIncomingRecord = normalizeEmptySubfieldsRecord(incomingRecord);
-  const normExistingRecord = normalizeEmptySubfieldsRecord(existingRecord);
+  //debug(`ic`);
+  const normIncomingRecord = normalizeRecord(incomingRecord);
+  //debug(`db`);
+  const normExistingRecord = normalizeRecord(existingRecord);
 
   if (deepEqual(normIncomingRecord, normExistingRecord) === true) { // eslint-disable-line functional/no-conditional-statement
 
@@ -73,3 +73,48 @@ export function validateChanges({incomingRecord, existingRecord, validate = true
 
   return {changeValidationResult: true};
 }
+
+function normalizeRecord(record) {
+
+  debug(`Normalizing record: empty subfields`);
+  //debugData(record);
+  // normalizeEmptySubfieldsRecord is an utility function which normalizes all all cases of empty subfield value {code: x, value: ""}, {code: x}, {code: x, value: undefined}  to {value: ""}
+  // this is needed, because there are CAT-fields that have empty value in subfield $b
+  // we could optimize this step out, if we could be sure that empty subfield values are normalized somewhere else
+
+  const normRecord1 = normalizeEmptySubfieldsRecord(record);
+  debug(`Normalizing record: f008/00-05`);
+  // Normalize f008/00-05 - In non-MARC21 imports f008 'Date entered on file' gets always the current date
+  // This propably should be configurable
+  //debugData(normRecord1);
+  const normRecord = emptyf008CreationDate(normRecord1);
+
+  return normRecord;
+}
+
+function emptyf008CreationDate(record) {
+  debugData(record);
+  const controlFields = record.getControlfields();
+  const normControlFields = controlFields.map(emptyf008);
+  //debugData(JSON.stringify(controlFields));
+
+  const dataFields = record.getDatafields();
+  const fields = normControlFields.concat(dataFields);
+  //debugData(fields);
+  return new MarcRecord({leader: record.leader, fields}, {subfieldValues: false});
+
+  function emptyf008(controlfield) {
+    if (controlfield.tag !== '008' || controlfield.value.length < 6) {
+      return controlfield;
+    }
+    return {tag: controlfield.tag, value: emptyCreationDate(controlfield.value)};
+  }
+
+  function emptyCreationDate(f008value) {
+    const newValue = `000000${f008value.substring(6)}`;
+    debug(`Emptied creationDate from ${f008value} to ${newValue}`);
+    return newValue;
+  }
+
+}
+
