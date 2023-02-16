9400ec124c417c1b9a5a54d1938a7f1334ecd818
Minttu Hurme <ammsalme@users.noreply.github.com>
2023-02-16 13:33:13 +0200

    Next (#112)
    * Dependency update (#110)
    
    1. Update to matching
    * New config for stdno candidate matchDetection (#114)
    
    2. Updates to merge
    * Update merge-reducers v1.1.0 (#111)
    * Update merge-reducers: v1.2.0-alpha.1 (#115)
    * Update merge-reducers: v1.2.0-alpha.2 (#116)
    
    3. Updates to validating changes in skipNoChangeUpdate -functionality
    * Skip changes to f008 creationDate (#117)
    * Better skip2 (#118)
    * Better skip3 (#119)
    
    * 3.2.0-alpha.2

--- src/interfaces/validator/validate-changes.js
+++ src/interfaces/validator/validate-changes.js
@@ -4,7 +4,7 @@
 *
 * RESTful API for Melinda - record validation services
 *
-* Copyright (C) 2022 University Of Helsinki (The National Library Of Finland)
+* Copyright (C) 2022-2023 University Of Helsinki (The National Library Of Finland)
 *
 * This file is part of melinda-rest-api-validator
 *
@@ -29,12 +29,15 @@
 import deepEqual from 'deep-eql';
 import {detailedDiff} from 'deep-object-diff';
 import createDebugLogger from 'debug';
-import {normalizeEmptySubfieldsRecord} from '../../utils';
+import {normalizeEmptySubfields} from '../../utils';
+import {MarcRecord} from '@natlibfi/marc-record';
+
+const debug = createDebugLogger('@natlibfi/melinda-rest-api-validator:validator:validate-changes');
+const debugData = debug.extend('data');
 
 // Checks that the incomingRecord and existingRecord are not identical
+// eslint-disable-next-line max-statements
 export function validateChanges({incomingRecord, existingRecord, validate = true}) {
-  const debug = createDebugLogger('@natlibfi/melinda-rest-api-validator:validator:validate-changes');
-  const debugData = debug.extend('data');
 
   if (validate === false) {
     debug(`Skipping validateChanges. Validate: ${validate}`);
@@ -42,20 +45,19 @@ export function validateChanges({incomingRecord, existingRecord, validate = true
   }
 
   // Optimize the check by first counting the fields
-  if (incomingRecord.fields.length !== existingRecord.fields.length) {
+  if (incomingRecord.fields.filter(isActualContentField).length !== existingRecord.fields.filter(isActualContentField).length) {
     debug(`validateChanges: OK - there are changes between incomingRecord and existingRecord`);
     debugData(`IncomingRecord field count: ${incomingRecord.fields.length}`);
+    debugData(`IncomingRecord field tags: ${incomingRecord.fields.map(field => field.tag)}`);
     debugData(`ExistingRecord field count: ${existingRecord.fields.length}`);
+    debugData(`ExistingRecord field tags: ${existingRecord.fields.map(field => field.tag)}`);
     return {changeValidationResult: true};
   }
 
-
-  // normalizeEmptySubfieldsRecord is an utility function which normalizes all all cases of empty subfield value {code: x, value: ""}, {code: x}, {code: x, value: undefined}  to {value: ""}
-  // this is needed, because there are CAT-fields that have empty value in subfield $b
-  // we could optimize this step out, if we could be sure that empty subfield values are normalized somewhere else
-
-  const normIncomingRecord = normalizeEmptySubfieldsRecord(incomingRecord);
-  const normExistingRecord = normalizeEmptySubfieldsRecord(existingRecord);
+  //debug(`ic`);
+  const normIncomingRecord = normalizeRecord(incomingRecord);
+  //debug(`db`);
+  const normExistingRecord = normalizeRecord(existingRecord);
 
   if (deepEqual(normIncomingRecord, normExistingRecord) === true) { // eslint-disable-line functional/no-conditional-statement
 
@@ -73,3 +75,123 @@ export function validateChanges({incomingRecord, existingRecord, validate = true
 
   return {changeValidationResult: true};
 }
+
+function isActualContentField(field) {
+  return field.tag !== '001' && field.tag !== '003' && field.tag !== '005';
+}
+
+function normalizeRecord(record) {
+  debugData(record);
+  const normLeader = emptyLeader(record.leader);
+
+  const normControlFields = record.getControlfields().filter(isActualContentField).map(emptyf008);
+  //const normControlFields = controlFields.map(emptyf008).filter(isActualContentField);
+  //debugData(JSON.stringify(controlFields));
+
+  // normalizeEmptySubfields is an utility function which normalizes all all cases of empty subfield value {code: x, value: ""}, {code: x}, {code: x, value: undefined}  to {value: ""}
+  // this is needed, because there are CAT-fields that have empty value in subfield $b
+  // we could optimize this step out, if we could be sure that empty subfield values are normalized somewhere else
+  const normDataFields = record.getDatafields().filter(isActualContentField).map(normalizeEmptySubfields);
+
+  const normFields = normControlFields.concat(normDataFields);
+  // Differences in the (tag) order in Aleph-internal fields are ignored
+  // Note that this does not ignore differences in field order inside same tag
+  debug(`Sort Aleph internal fields`);
+  const sortedNormFields = sortAlephInternalFields(normFields);
+  debugData(`OrigTags: ${normFields.map(field => field.tag)}`);
+  debugData(`SortTags: ${sortedNormFields.map(field => field.tag)}`);
+
+  //debugData(fields);
+  return new MarcRecord({leader: normLeader, fields: sortedNormFields}, {subfieldValues: false});
+
+  function emptyLeader(leader) {
+    if (!ldrIsValid(leader)) {
+      return leader;
+    }
+    // emptyLeader:
+    // - Normalize away LDR values that are 00-04 and 12-16 which are relevant only in binary MARC 21
+    // - Normalize away n/c difference in LDR/05
+    const normLeader1 = normalizeLdr05(leader);
+    const normLeader2 = emptyLdrBinaryValues(normLeader1);
+    return normLeader2;
+  }
+
+  function ldrIsValid(leader) {
+    if (!typeof leader === 'string' && !(leader instanceof String)) {
+      debug(`Non-string LDR, cannot normalize.`);
+      return false;
+    }
+    if (leader.length !== 24) {
+      debug(`Weird LDR length, cannot normalize`);
+      return false;
+    }
+    return true;
+  }
+
+  function normalizeLdr05(leader) {
+    debug(`Normalize LDR/05`);
+    const newLeader = `${leader.substring(0, 5)}${normLdr05Value(leader.substring(5, 6))}${leader.substring(6)}`;
+    debugData(`LdrOrig: ${leader}`);
+    debugData(`LdrNorm: ${newLeader}`);
+    return newLeader;
+  }
+
+  function normLdr05Value(value) {
+    if (value === 'n') {
+      return 'c';
+    }
+    return value;
+  }
+
+  function emptyLdrBinaryValues(leader) {
+    debug(`Empty LDR binary values`);
+    const newLeader = `00000${leader.substring(5, 12)}00000${leader.substring(17)}`;
+    debugData(`LdrOrig: ${leader}`);
+    debugData(`LdrNorm: ${newLeader}`);
+    return newLeader;
+  }
+
+  function emptyf008(controlfield) {
+    if (controlfield.tag !== '008' || controlfield.value.length < 6) {
+      return controlfield;
+    }
+    return {tag: controlfield.tag, value: emptyCreationDate(controlfield.value)};
+  }
+
+  function emptyCreationDate(f008value) {
+  // emptyCreationDate:
+  // Normalize f008/00-05 - In non-MARC21 imports f008 'Date entered on file' gets always the current date
+  // This propably should be configurable
+    debug(`Empty 008 creationDate.`);
+    const newValue = `000000${f008value.substring(6)}`;
+    debugData(`f008Orig: ${f008value}`);
+    debugData(`f008Norm: ${newValue}`);
+    return newValue;
+  }
+
+  // Sort Aleph internal fields (tags consisting letters) to alphabetical tag order
+  // We should have also tag-internal sort for - these are sane order in (almost) all cases for handling LOW/SID/CAT:
+  // LOW: $a contents
+  // SID: $b contents
+  // CAT: $c+$h contents
+
+  function sortAlephInternalFields(fields) {
+    const alephInternalPattern = /^[A-Z][A-Z][A-Z]$/u;
+    return [...fields].sort((a, b) => {
+      // If either of fields is and internal field do sort
+      if (alephInternalPattern.test(a.tag) || alephInternalPattern.test(b.tag)) {
+        if (a.tag > b.tag) {
+          return 1;
+        }
+        if (b.tag > a.tag) {
+          return -1;
+        }
+        return 0;
+      }
+      // if neither field is an internal field do not sort
+      return 0;
+    });
+  }
+
+}
+
