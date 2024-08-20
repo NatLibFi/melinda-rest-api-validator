97ed334a2ac3b75c7b67710494c2a0fcf30c27b3
Minttu Hurme <ammsalme@users.noreply.github.com>
2024-08-20 19:14:26 +0300

    Dependency  & Maintenance update (#432) (#433) (#434)
    * Dependency  & Maintenance update (#432)
    
    * Bump braces from 3.0.2 to 3.0.3 (#406)
    
    * Bump the production-dependencies group with 8 updates (#430)
    
    Bumps the production-dependencies group with 8 updates:
    
    | Package | From | To |
    | --- | --- | --- |
    | [@natlibfi/marc-record](https://github.com/natlibfi/marc-record-js) | `9.0.0` | `9.0.1` |
    | [@natlibfi/marc-record-merge](https://github.com/natlibfi/marc-record-merge-js) | `7.0.3` | `7.0.5` |
    | [@natlibfi/marc-record-validate](https://github.com/natlibfi/marc-record-validate) | `8.0.8` | `8.0.10` |
    | [@natlibfi/melinda-backend-commons](https://github.com/natlibfi/melinda-backend-commons-js) | `2.3.0` | `2.3.1` |
    | [@natlibfi/melinda-commons](https://github.com/natlibfi/melinda-commons-js) | `13.0.15` | `13.0.16` |
    | [@natlibfi/melinda-record-match-validator](https://github.com/NatLibFi/melinda-record-match-validator) | `2.2.2` | `2.2.3` |
    | [@natlibfi/melinda-rest-api-commons](https://github.com/natlibfi/melinda-rest-api-commons) | `4.1.7` | `4.1.8` |
    | [@natlibfi/sru-client](https://github.com/natlibfi/sru-client-js) | `6.0.12` | `6.0.13` |
    
    
    * Bump the development-dependencies group with 3 updates (#431)
    
    Bumps the development-dependencies group with 3 updates: [@natlibfi/fixugen-http-server](https://github.com/natlibfi/fixugen-http-server-js), [@natlibfi/fixura](https://github.com/natlibfi/fixura-js) and [mocha](https://github.com/mochajs/mocha).
    
    * Update license to MIT (#433)
    
    * Update deps
    
    * Add missing deps: debug, fixugen
    
    * Remove unused deps: validate, marc-record-validators-melinda
    
    * Remove unused dep: chai-http
    
    * Remove unused deps: moment, chai-http
    
    * Remove unused dep: fixugen-http-server
    
    * Remove unused dep: mock-fs
    
    * Update nyc & istanbul
    
    * Node-tests: remove v19, add v22
    
    * 3.6.3-alpha.2

--- src/interfaces/validator/validate-existing-record.spec.js
+++ src/interfaces/validator/validate-existing-record.spec.js
@@ -1,30 +1,3 @@
-/**
-*
-* @licstart  The following is the entire license notice for the JavaScript code in this file.
-*
-* Melinda record matching modules for Javascript
-*
-* Copyright (C) 2020 University Of Helsinki (The National Library Of Finland)
-*
-* This file is part of melinda-record-matching-js
-*
-* melinda-record-matching-js program is free software: you can redistribute it and/or modify
-* it under the terms of the GNU Lesser General Public License as
-* published by the Free Software Foundation, either version 3 of the
-* License, or (at your option) any later version.
-*
-* melinda-record-matching-js is distributed in the hope that it will be useful,
-* but WITHOUT ANY WARRANTY; without even the implied warranty of
-* MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-* GNU Lesser General Public License for more details.
-*
-* You should have received a copy of the GNU Affero General Public License
-* along with this program.  If not, see <http://www.gnu.org/licenses/>.
-*
-* @licend  The above is the entire license notice
-* for the JavaScript code in this file.
-*
-*/
 
 import generateTests from '@natlibfi/fixugen';
 import {READERS} from '@natlibfi/fixura';
